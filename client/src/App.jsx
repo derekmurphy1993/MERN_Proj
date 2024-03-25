22d05236b265cd3dcd9ff3bc87bac5d6900cba11
Derek <56985040+derekmurphy1993@users.noreply.github.com>
2024-03-25 12:56:18 -0400

    Create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SignIn from './pages/SignIn';
+import Profile from './pages/Profile';
+import Home from './pages/Home';
+import SignUp from './pages/SignUp';
+import About from './pages/About';
+
 
 export default function App() {
-  return (
-    <div>App
-
-
-
-      
-    </div>
-  )
+  return <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/signin" element={<SignIn />} />
+    <Route path="/signup" element={<SignUp />} />
+    <Route path="/about" element={<About />} />
+    <Route path="/profile" element={<Profile />} />
+  </Routes>
+  </BrowserRouter>
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,3 +1,4 @@
+/** @type {import(tailwindcss').config;} */
 export default {
   content: [
     "./index.html",
