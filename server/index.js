1b5b32fa975b2a6279e2be7e5a7f02f8519ae250
Derek <56985040+derekmurphy1993@users.noreply.github.com>
2024-03-26 14:28:25 -0400

    [Feat] Connected to MongoDB

--- server/index.js
+++ server/index.js
@@ -1,8 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('Connected to mongoDB');
+}).catch((err) => {
+    console.log(err)
+});
+
 app.listen(3000, ()=> {
-    console.log('server is running on port 3000!')
+    console.log('server is running on port 3000!!!')
 });
 
