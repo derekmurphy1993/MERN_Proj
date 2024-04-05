7449ca461ea2e1f6babfa37a30f697fb967cc011
Derek <56985040+derekmurphy1993@users.noreply.github.com>
2024-04-05 13:45:45 -0400

    [feat] Updated the Header to handle Sign In permissions

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import Home from './pages/Home';
 import SignUp from './pages/SignUp';
 import About from './pages/About';
 import Header from './components/Header';
+import PrivRoute from './components/PrivRoute';
 
 
 export default function App() {
@@ -16,7 +17,9 @@ export default function App() {
     <Route path="/signin" element={<SignIn />} />
     <Route path="/signup" element={<SignUp />} />
     <Route path="/about" element={<About />} />
+    <Route element={<PrivRoute />}>
     <Route path="/profile" element={<Profile />} />
+    </Route>
   </Routes>
   </BrowserRouter>
 }
