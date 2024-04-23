cf35e34af455430a6daf64c56889511d5cb8a66b
Derek <56985040+derekmurphy1993@users.noreply.github.com>
2024-04-23 10:04:06 -0400

    [FEAT] Added delete listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -129,6 +129,27 @@ export default function Profile() {
     }
   };
 
+  const handleListingDelete = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listings/delete/${listingId}`, {
+        method: 'DELETE',
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        return;
+      }
+
+      setUserListing((prev) => prev.filter((listing) => listing._id !== listingId));
+
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
+
+  const handleListingUpdate = () => {
+    
+  }
+
   return (
     <div className="p3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7"> Profile </h1>
@@ -184,8 +205,8 @@ export default function Profile() {
         </Link>
 
         <div className='flex flex-col items-center'>
-          <button className='text-red-700 uppercase'>Delete</button>
-          <button className='text-green-700 uppercase'>Edit</button>
+          <button onClick={()=>handleListingDelete(listing._id)} className='text-red-700 uppercase'>Delete</button>
+          <button onClick={console.log("clicked")} className='text-green-700 uppercase'>Edit</button>
         </div>
         </div>
       ))}
