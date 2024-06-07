6d8c0ec4e7db7032bfa57e29bfb95be1becb8f8f
Derek <56985040+derekmurphy1993@users.noreply.github.com>
2024-06-07 13:03:02 -0500

    [feat] Added about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About <span className='text-green-600'>Rentopia</span></h1>
+      <p className='mb-4 text-slate-700'>Finding a reliable and easy rental real estate service can transform the daunting process of securing a home into a seamless experience. Such a service prioritizes user-friendliness, offering intuitive online platforms where potential tenants can browse through a variety of listings, filter by preferences such as location, price, and amenities, and even take virtual tours. The ease of navigating the platform ensures that users can quickly find properties that meet their needs without feeling overwhelmed by excessive options or complex search processes. Additionally, clear, high-quality photos and detailed descriptions further aid in making informed decisions.</p>
+      <p className='mb-4 text-slate-700'>Another significant feature of an easy rental real estate service is the streamlined application and approval process. Traditional rental applications often involve substantial paperwork and long waiting periods, but a modern service simplifies this by offering online applications that can be completed and submitted in minutes. These platforms often integrate background checks and credit assessments directly into the application process, providing landlords with the necessary information quickly and securely. By reducing the time and hassle associated with applications, both tenants and landlords benefit from a more efficient rental process.</p>
+      <p className='mb-4 text-slate-700'>Customer support is also a crucial aspect of a top-tier rental real estate service. Users should have access to responsive and helpful customer service representatives who can assist with any issues or questions that arise. This could include help with understanding lease terms, resolving maintenance requests, or navigating the platform. Offering multiple channels of support, such as phone, email, and live chat, ensures that help is readily available whenever it's needed. Ultimately, the goal of an easy rental real estate service is to provide a hassle-free, efficient, and supportive experience for all parties involved in the rental transaction.</p>
+    </div>
   )
 }
